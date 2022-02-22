a8378d041d0ef886a1702dc7ec01dd263acf7b65
zhengzeyao <zeyaoyo@gmail.com>
2022-02-22 15:55:33 -0800

    Assignment 4 -- protocol and delegate

---
+++ Assignment4_Protocol+Delegate/Assignment4_Protocol+Delegate/GetNameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GetNameViewController.swift
+//  Assignment4_Protocol+Delegate
+//
+//  Created by 郑泽瑶 on 2/21/22.
+//
+
+import UIKit
+
+protocol SendNameDelegate {
+    func setName(firstName: String, lastName: String)
+    func setWelcomeText(welcomeText : String)
+}
+
+class GetNameViewController: UIViewController {
+
+    @IBOutlet weak var txtFirst: UITextField!
+    
+    @IBOutlet weak var txtLast: UITextField!
+    
+    var sendNameDelegate : SendNameDelegate?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func save(_ sender: Any) {
+        guard let first = txtFirst.text else {return}
+        guard let last = txtLast.text else {return}
+        sendNameDelegate?.setName(firstName: first, lastName: last)
+        sendNameDelegate?.setWelcomeText(welcomeText: "Welcome \(first),\(last)")
+
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
---
+++ Assignment4_Protocol+Delegate/Assignment4_Protocol+Delegate/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Assignment4_Protocol+Delegate
+//
+//  Created by 郑泽瑶 on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SendNameDelegate {
+    
+
+    @IBOutlet weak var lblFirst: UILabel!
+    
+    @IBOutlet weak var lblLast: UILabel!
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getName(_ sender: Any) {
+        performSegue(withIdentifier: "segueSendName", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueSendName" {
+            let secondVC = segue.destination as! GetNameViewController
+            secondVC.sendNameDelegate = self
+        }
+    }
+    
+    
+    func setName(firstName: String, lastName: String) {
+        lblFirst.text = firstName
+        lblLast.text = lastName
+    }
+    
+    func setWelcomeText(welcomeText: String) {
+        lblWelcome.text = welcomeText
+    }
+}
+
